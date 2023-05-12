530eafea7744cb55115be399eeb3990ed44bc893
Reka <reka.kovacs1993@gmail.com>
2023-05-12 14:24:59 +0100

    pangram finder

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,12 @@
 const PangramFinder = function (phrase) {
-  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.toUpperCase().split('');
+  this.phrase = phrase.toUpperCase().split("")
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  return this.alphabet.every((letter) => {
+    return this.phrase.includes(letter)
+  })
 }
 
 module.exports = PangramFinder;
