804e49178fda31bf05448248559b0f9fffd45330
Reka <reka.kovacs1993@gmail.com>
2023-05-12 14:57:24 +0100

    isogram finder

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,11 @@
 const IsogramFinder = function (word) {
-
+    this.word = word.split("")
 }
 
 IsogramFinder.prototype.isIsogram = function () {
-
+    return !this.word.some((letter, i) => {
+        return this.word.includes(letter, i+ 1)
+    })
 }
 
 module.exports = IsogramFinder;
